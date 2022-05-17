847866560c0185d06d4f05dc29c58e6e05c37edf
choeqq <k.chola@op.pl>
2022-05-17 22:38:24 +0200

    some added

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,14 @@ function reduce<T>(array: T[], cb: Function, initialValue: any) {
   return currentValue;
 }
 
+function some<T>(array: T[], cb: Function) {
+  for (let i = 0; i < array.length; i++) {
+    if (cb(array[i], i, array)) return true;
+  }
+
+  return false;
+}
+
 module.exports = {
   forEach,
   map,

f84ed88963f2fdd56a0d0a157a204a545ed9ac38
choeqq <k.chola@op.pl>
2022-05-19 23:06:29 +0200

    for loop solution

--- src/asyncAwaitHandler.js
+++ src/asyncAwaitHandler.js
@@ -1,7 +1,7 @@
 const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const initApp = async () => {
-  useForEach(ids);
+  getPostSerialized(ids);
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
@@ -12,9 +12,17 @@ const getPost = async (id) => {
   ).json();
 };
 
-const useForEach = (ids) => {
-  ids.forEach(async (id) => {
-    const data = await getPost(id);
+// const useForEach = (ids) => {
+//   ids.forEach(async (id) => {
+//     const data = await getPost(id);
+//     console.log(data);
+//   });
+// };
+
+const getPostSerialized = async (ids) => {
+  for (let i = 0; i < ids.length; i++) {
+    const data = await getPost(ids[i]);
     console.log(data);
-  });
+  }
+  console.log("I will wait 4u");
 };
